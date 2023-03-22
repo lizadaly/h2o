9d9448d3b20d9a0b52c8a2e18970099b507d82fb
Liza Daly <lizadaly@gmail.com>
2023-03-22 17:06:59 -0400

    Streamline search results by opting out of Vuex

---
+++ web/frontend/components/Search/LegalDocumentSearch.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <search-form />
+        <result-form />
+    </section>
+</template>
+
+<script>
+import SearchForm from './SearchForm'; 
+import ResultForm from './ResultForm';
+
+export default {
+    components: {
+        SearchForm,
+        ResultForm
+    }
+}
+</script>
+
